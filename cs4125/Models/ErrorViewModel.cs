baec8df6977087f6d45d2737480334cd941689cf
App-lied <alanphillips2001@gmail.com>
2022-10-24 22:39:54 +0100

    Add project files.

---
+++ cs4125/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cs4125.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
