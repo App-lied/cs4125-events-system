ed66708bb9b28d0157b47a02c9ba1a5b6464826a
SEAN GOTHARD <19265107@studentmail.ul.ie>
2022-10-27 15:42:37 +0100

    Added Controllers and connect them to Views

---
+++ cs4125/Controllers/InformationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cs4125.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Information()
+        {
+            return View();
+        }
+    }
+}
