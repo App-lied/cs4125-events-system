6e4698390aaa75bc4df64dca30269d18a813e284
App-lied <alanphillips2001@gmail.com>
2022-11-20 19:54:02 +0000

    Completed and comments Discount classes

--- cs4125/Models/Discount.cs
+++ cs4125/Models/Discount.cs
@@ -1,9 +1,16 @@
 ﻿namespace cs4125.Models
 {
+    /// <summary>
+    /// Interface abstracting a discount campaign for an event.
+    /// </summary>
     public interface IDiscount
     {
         abstract double getDiscount(User cust, Event ev);
     }
+
+    /// <summary>
+    /// Class <c>AgeDiscount</c> models a discount based on a specified age limit. If the customer's age is above the limit, the discount is applied.
+    /// </summary>
     public class AgeDiscount : IDiscount {
         protected int ageLimit;
         protected double discount;
@@ -23,6 +30,9 @@
         }
     }
 
+    /// <summary>
+    /// Class <c>FirstTicketsDiscount</c> models a discount based on the first N tickets for an event. If the purchased is within the limit, the discount is applied.
+    /// </summary>
     public class FirstTicketsDiscount : IDiscount{
         protected int ticketLimit;
         protected double discount;
@@ -35,6 +45,10 @@
 
         public double getDiscount(User cust, Event ev)
         {
+            if(ev.RemainingTickets > ev.Tickets.Count - ticketLimit) { 
+                return discount; 
+            }
+
             return 0.0;
         }
     }
