664e9c12037e5defeeae291e48fbd964a24d0aee
ConorMcNelis <60985221+ConorMcNelis@users.noreply.github.com>
2022-12-02 12:50:42 +0000

    Added Event Organiser CSV Write

--- cs4125/Controllers/RegisterController.cs
+++ cs4125/Controllers/RegisterController.cs
@@ -19,14 +19,40 @@ namespace cs4125.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(IFormCollection collection)
         {
+            //reads in the form details
             var email = collection["registrationEmail"];
             var password = collection["registrationPassword"];
             var password2 = collection["registrationPassword2"];
             var name = collection["registrationName"];
             var premium = collection["registrationPremium"];
+            var eventorganiser = collection["registrationOrganiser"];
             DateTime birth = DateTime.Parse(collection["registrationDate"]);
+
+            //checks the passwords match
             if (password == password2)
             {
+                if (eventorganiser == "on")
+                {
+                    //creates Event Organsier factory
+                    EventOrganiserFactory userFevent = new EventOrganiserFactory();
+                    //uses factory method to create Event Organiser with the Profile Type Enum
+                    Profile profile = userFevent.GetProfile(ProfileType.EventOrganiser, email, password, name, birth);
+                    //Checks if the age is greater than 16 and returns error if not
+                    if (profile.getAge() <= 16)
+                    {
+                        ViewBag.error = "Profile not created: Must be over 16 years old";
+                        return View();
+                    }
+                    else
+                    {
+                        //writes the information to the login information csv and sets the singleton to the registered user
+                        profile.writeInfoToCSV();
+                        LoggedInUser login = LoggedInUser.GetInstance(email, password, name);
+                        return RedirectToAction("Profile", "Profile");
+                    }
+                }
+
+                //follows the same format as event organiser factory but checks if the account is premimum or not and acts accordingly
                 UserFactory userF = new UserFactory();
                 if (premium == "on")
                 {
